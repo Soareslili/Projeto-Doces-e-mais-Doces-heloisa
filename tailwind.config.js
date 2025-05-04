2b9537011f1c45181869a8c3e679efde024185ad
Soareslili <lidianesantossoares2@gmail.com>
2025-05-04 17:55:44 -0300

    Criando a parte principal do projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,23 @@ module.exports = {
       fontFamily: {
         roboto: ['Roboto', 'sans-serif'],
       },
+      animation: {
+        'up-down': 'upDown 3s ease-in-out infinite',
+      },
+      keyframes: {
+        upDown: {
+          '0%': {
+            transform: 'translateY(0)',
+          },
+          '50%': {
+            transform: 'translateY(-20px)', 
+          },
+          '100%': {
+            transform: 'translateY(0)',
+          },
+        },
+      },
     },
   },
   plugins: [],
 }
-
